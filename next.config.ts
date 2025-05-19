6cdd4bc0f4ad13e5c28337e9fb998179977d271e
lucashrqc <lucas.hrqc@gmail.com>
2025-05-19 18:20:17 -0300

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/react-memory-game',
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
 };
 
